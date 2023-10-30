dd636aaef937efa54d1cf661e62e341b91384e50
nplieger <plieger05@gmail.com>
2023-10-30 12:23:23 +0100

    Remove comments

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -6,7 +6,7 @@
 /*   By: mfaucheu <mfaucheu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/14 11:48:29 by nicolas           #+#    #+#             */
-/*   Updated: 2023/10/30 12:07:58 by nicolas          ###   ########.fr       */
+/*   Updated: 2023/10/30 12:23:12 by nicolas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -157,15 +157,10 @@ class	Server
 		void			pass(const t_commandParams &commandParams);
 
 		// Command utilities
-		//void			putMessage(Client *client, const std::string &delimiter, size_t &pos);
 		bool			isCommand(const std::string &clientBuffer);
 		bool			verifyServerPermissions(const Client *client, const int &mask);
 		void			errCommand(const Client *client, const std::string &code,
 							const std::string &parameter, const std::string &trailing);
-		//void			serverResponse(const Client *client, const std::string &code,
-		//					const std::string &parameters, const std::string &trailing);
-		//void			commandResponse(const Client *client, const std::string &command,
-		//					const void *target, const std::string &trailing);
 
 		// Getters
 		Client				*getClient(const std::string &nickname);
