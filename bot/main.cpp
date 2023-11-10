93d1b0180b058353a513fd9807ae0d70a5b295eb
HaniaKhater <hania.khater2@hotmail.com>
2023-11-10 12:14:26 +0100

    added a 6sec pause between the set-up and the punchline and the signal handeling

--- bot/main.cpp
+++ bot/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hania <hania@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:34:09 by hania             #+#    #+#             */
-/*   Updated: 2023/11/09 20:56:30 by hania            ###   ########.fr       */
+/*   Updated: 2023/11/10 12:12:08 by hania            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <cstdlib>
+#include <signal.h>
+
+bool	bot_end = false;
+
+void	signalHandler(int sig) {
+	(void)sig;
+	bot_end = true;
+}
 
 void	send_msg(int sd, std::string msg) {
 	msg += "\r\n";
@@ -28,11 +36,13 @@ void	send_msg(int sd, std::string msg) {
 	std::cout << "Sent: " << msg << std::endl;
 }
 
-// ./bot <server_port> <password> <nickname> <channel>
 int	main(int ac, char **av)
 {
 	if (ac != 5)
+	{
+		std::cerr << "Invalid input. Please try ./bot <server_port> <password> <nickname> <channel>" << std::endl;
 		return (0);
+	}
     int irc_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in server_addr;
     server_addr.sin_family = AF_INET;
@@ -45,21 +55,27 @@ int	main(int ac, char **av)
     send_msg(irc_socket, "NICK " + (std::string)av[3]);
     send_msg(irc_socket, "USER Bot * * :Mr Bot");
     send_msg(irc_socket, "JOIN #" + (std::string)av[4]);
-	while (1)
+
+	signal(SIGINT, signalHandler);
+	while (!bot_end)
 	{
-		std::ifstream	botFile("./input.config");
+		std::ifstream	botFile("./bot.config");
 		std::string		line;
 		int				line_nb;
+		int				pause;
 
 		if (!botFile.is_open())
 			std::cerr << "Unable to open the bot's input file" << std::endl;
 		srand(time(NULL));
-		line_nb = std::rand() % 31 + 1;
+		line_nb = std::rand() % 44 + 1;
 		for (int i = 0; i <= line_nb; i++)
 		{
 			getline(botFile, line);
 		}
-		send_msg(irc_socket, "PRIVMSG #h : " + line);
-		sleep(20);
+		pause = line.find("... ");
+		send_msg(irc_socket, "PRIVMSG #h : " + line.substr(0, pause + 3));
+		sleep(7);
+		send_msg(irc_socket, "PRIVMSG #h : " + line.substr(pause, line.length()));
+		sleep(23);
 	}
 }
