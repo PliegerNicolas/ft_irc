46f31eb2cf8bb8bf85eb5b54a24ea65389d2af99
HaniaKhater <hania.khater2@hotmail.com>
2023-11-11 10:34:55 +0100

    now only sends a joke if prompted

--- bot/bot.cpp
+++ bot/bot.cpp
@@ -6,7 +6,7 @@
 /*   By: hania <hania@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:34:09 by hania             #+#    #+#             */
-/*   Updated: 2023/11/10 21:55:27 by hania            ###   ########.fr       */
+/*   Updated: 2023/11/11 10:32:49 by hania            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,9 +43,9 @@ std::string	recv_msg(int sd, bool registered)
 
 	struct timeval tv;
 	if (registered == true)
-		tv.tv_sec = 5;
+		tv.tv_sec = 600;
 	else
-		tv.tv_sec = 1;
+		tv.tv_sec = 2;
 	tv.tv_usec = 0;
 	setsockopt(sd, SOL_SOCKET, SO_RCVTIMEO, (const char *)&tv, sizeof tv);
 	usleep(1000);
@@ -56,6 +56,27 @@ std::string	recv_msg(int sd, bool registered)
 	}
 	buffer[bytes_received] = '\0';
 	return (std::string(buffer));
+}
+
+void	sendJoke(int sd, std::string channel) {
+	std::string		line;
+	int				line_nb;
+	int				pause;
+	std::ifstream	botFile("./config/bot.config");
+
+	if (!botFile.is_open()) {
+		std::cerr << "Unable to open the bot's input file" << std::endl;
+		return;
+	}
+	srand(time(NULL));
+	line_nb = std::rand() % 48 + 1;
+	for (int i = 0; i <= line_nb; i++) {
+		getline(botFile, line);
+	}
+	pause = line.find("... ");
+	send_msg(sd, "PRIVMSG #" + channel + " :" + line.substr(0, pause + 3));
+	sleep(7);
+	send_msg(sd, "PRIVMSG #" + channel + " :" + line.substr(pause, line.length()));
 }
 
 int	main(int ac, char **av)
@@ -69,57 +90,35 @@ int	main(int ac, char **av)
 		std::cerr << "Error creating socket." << std::endl;
 		return 1;
 	}
-
 	struct sockaddr_in server_addr;
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(6667);
+	server_addr.sin_port = htons(6668);
 	server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	if (connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr) == -1)) {
-		std::cerr << "Error connecting to the server." << std::endl;
-		close(server_socket);
-		return 1;
-	}
+	connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr));
+	// if (connect(server_socket, (struct sockaddr*)&server_addr, sizeof(server_addr) == -1)) {
+	// 	std::cerr << "Error connecting to the server: " << errno << std::endl;
+	// 	close(server_socket);
+	// 	return 1;
+	// }
 	std::string	nickname = (std::string)av[3];
 	std::string	channel = (std::string)av[4];
 
 	send_msg(server_socket, "PASS " + (std::string)av[2]);
 	send_msg(server_socket, "NICK " + nickname);
-	while (!recv_msg(server_socket, 0).find("You are now known as")) {
+	while (!recv_msg(server_socket, 1).find("You are now known as")) {
 		nickname += "_";
 		send_msg(server_socket, "NICK " + nickname);
 		sleep(1);
 	}
-	send_msg(server_socket, "USER Bot * * :Mr Bot");
-	recv_msg(server_socket, 1);
+	send_msg(server_socket, "USER Bot * * :Mr. Bot");
+	recv_msg(server_socket, 0);
 	send_msg(server_socket, "JOIN #" + channel);
 	send_msg(server_socket, "PRIVMSG #" + channel + " :Hello! my name is " + nickname + ". Send me a message if you want to hear a programming joke :)");
-	recv_msg(server_socket, 1);
+	recv_msg(server_socket, 0);
 
 	signal(SIGINT, signalHandler);
-	while (!bot_end)
-	{
-		if (!recv_msg(server_socket, 1).find("localhost"))
-		{
-			std::ifstream	botFile("./config/bot.config");
-			std::string		line;
-			int				line_nb;
-			int				pause;
-
-			if (!botFile.is_open())
-			{
-				std::cerr << "Unable to open the bot's input file" << std::endl;
-				return (0);
-			}
-			srand(time(NULL));
-			line_nb = std::rand() % 48 + 1;
-			for (int i = 0; i <= line_nb; i++)
-			{
-				getline(botFile, line);
-			}
-			pause = line.find("... ");
-			send_msg(server_socket, "PRIVMSG #" + channel + " :" + line.substr(0, pause + 3));
-			sleep(7);
-			send_msg(server_socket, "PRIVMSG #" + channel + " :" + line.substr(pause, line.length()));
-		}
+	while (!bot_end) {
+		if (recv_msg(server_socket, 1).find("PRIVMSG " + nickname))
+			sendJoke(server_socket, channel);
 	}
 }
